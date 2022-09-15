15021399229e1917325b07857dfadea01ae862b2
mohsen-hussen <mohsenhussen8@gmail.com>
2022-09-15 16:26:54 +0200

    add partners section

--- js/main.js
+++ js/main.js
@@ -11,4 +11,32 @@ $(document).ready(() => {
 			this.className += " activNavLink";
 		});
 	}
+
+	var swiper = new Swiper(".mySwiper", {
+		slidesPerView: 1,
+		spaceBetween: 20,
+		centeredSlides: true,
+		// autoplay: {
+		// 	delay: 2500,
+		// 	disableOnInteraction: false,
+		// },
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		breakpoints: {
+			640: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+			},
+			768: {
+				slidesPerView: 4,
+				spaceBetween: 40,
+			},
+			1024: {
+				slidesPerView: 5,
+				spaceBetween: 50,
+			},
+		},
+	});
 });
